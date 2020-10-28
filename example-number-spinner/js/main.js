3d5d370ed032716bffc870ae4975b7ebbfa83d3c
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2020-10-28 15:54:25 +0100

    final version of number spinner

--- example-number-spinner/js/main.js
+++ example-number-spinner/js/main.js
@@ -1,31 +1,88 @@
-
-
 $('.number-spinner').mousedown(function(e) {
 
   // 1. Beim Drücken der Maustaste -> Koordinaten merken
   let clickedElement = this;
   let clickX = e.clientX;
   let clickY = e.clientY;
-  let clickValue = parseFloat($(clickedElement).text());
+  let actValue = parseFloat($(clickedElement).text());
+
+  let min = parseFloat($(clickedElement).attr('data-min'));
+  let max = parseFloat($(clickedElement).attr('data-max'));
+
+  let speed = parseFloat($(clickedElement).attr('data-speed'));
+  let decimals = parseFloat($(clickedElement).attr('data-decimals'));
 
   // 2. Ab dann ständig schauen, wo die Maus ist.
   $(window).mousemove(function(e) {
     let actX = e.clientX;
+    let actY = e.clientY;
+
     let distX = actX - clickX;
-    let value = parseFloat($(clickedElement).text());
+
+    // normale Berechung
+    let newValue = actValue + distX * speed;
+    $(clickedElement).removeClass('fast slow');
     
-    let newValue = clickValue + distX;
+    if (actY < clickY - 20) {
+      newValue = actValue + distX * speed * 10;
+      $(clickedElement).addClass('fast');
+
+    } else if (actY > clickY + 20) {
+      newValue = actValue + distX * speed / 10;
+      $(clickedElement).addClass('slow');
     
+    } 
+
+    actValue = newValue;
+    clickX = actX;
+
+    actValue = Math.min(actValue, max);
+    actValue = Math.max(actValue, min);
+
+    //displayedValue = Math.round(actValue);
+    displayedValue = actValue.toFixed(decimals);
+
+    // displayedValue = Math.min(displayedValue, max);
+    // displayedValue = Math.max(displayedValue, min);
+
     // 3. Zahl der Number Spinners entsprechend verändern.
-    $(clickedElement).text(newValue);
+    $(clickedElement).text( displayedValue );
   });
 
   // 4. Beim Loslassen der Maustaste (egal wo) -> EventListener mousemove eliminieren!
   $(window).mouseup(function(e) {
     $(window).off('mousemove');
+    $(window).off('mouseup');
+
+    $(clickedElement).removeClass('fast slow');
   });
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
