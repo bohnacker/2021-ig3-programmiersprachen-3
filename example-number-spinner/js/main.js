fc6127e426fbfd35698930efbf095f9612f96ebc
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2020-10-28 13:21:31 +0100

    Minor changes and comments

--- example-number-spinner/js/main.js
+++ example-number-spinner/js/main.js
@@ -1,16 +1,14 @@
 
-// 1. Bei Klick -> Koordinaten merken
-// 2. Ab dann ständig schauen, wo die Maus ist.
-// 3. Zahl der Number Spinners entsprechend verändern.
-// 4. Beim Loslassen der Maustaste (egal wo) -> EventListener mousemove eliminieren!
-
 
 $('.number-spinner').mousedown(function(e) {
+
+  // 1. Beim Drücken der Maustaste -> Koordinaten merken
   let clickedElement = this;
   let clickX = e.clientX;
   let clickY = e.clientY;
   let clickValue = parseFloat($(clickedElement).text());
 
+  // 2. Ab dann ständig schauen, wo die Maus ist.
   $(window).mousemove(function(e) {
     let actX = e.clientX;
     let distX = actX - clickX;
@@ -18,9 +16,11 @@ $('.number-spinner').mousedown(function(e) {
     
     let newValue = clickValue + distX;
     
+    // 3. Zahl der Number Spinners entsprechend verändern.
     $(clickedElement).text(newValue);
   });
 
+  // 4. Beim Loslassen der Maustaste (egal wo) -> EventListener mousemove eliminieren!
   $(window).mouseup(function(e) {
     $(window).off('mousemove');
   });
